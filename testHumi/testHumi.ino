e54cbf9363c502ccce1ed78cd97abd1c59a85183
KACHINROT\Rachaneewan <rachaning02@gmail.com>
2017-04-14 17:59:33 +0700

    Edit code mcu 2 mqtt

--- testHumi/testHumi.ino
+++ testHumi/testHumi.ino
@@ -1,34 +1,74 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include "DHT.h"
 
 #define DHTPIN D2 // what pin we're connected to
-
-// Uncomment whatever type you're using!
 #define DHTTYPE DHT11 // DHT 11 
-//#define DHTTYPE DHT22 // DHT 22 (AM2302)
-//#define DHTTYPE DHT21 // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+//config wifi
+const char* ssid = "ARADA"; // wifi ssid
+const char* password = "0815350228"; // wifi้ password
+
+// Config MQTT Server
+const char * topic = "keppa"; // name fo topic 
+char message_buff[100];
+#define mqtt_server "broker.hivemq.com" // server
+#define mqtt_port 1883   // เลข port
+#define mqtt_user "555555666777" // user
+#define mqtt_password "" // password
+
+// MQTT Server address
+WiFiClient keppa;
+PubSubClient client(keppa);
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
 Serial.begin(9600); 
 Serial.println("DHTxx test!");
-
 dht.begin();
+WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }//while
+  // จบการเชื่อมต่อ network
+  
+  if (client.connect("555555666777")) {
+    client.publish(topic,"Keppa si!!!");
+    client.subscribe(topic);
+    }
+    client.setServer(mqtt_server, mqtt_port); // เชื่อมต่อ mqtt server
 }
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("555555666777")) {
+    Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(topic, "hello world");
+      // ... and resubscribe
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }//else
+  }//while
+}//reconnect
+
 void loop() {
-// Reading temperature or humidity takes about 250 milliseconds!
-// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
    float h = dht.readHumidity();
    float t = dht.readTemperature();
-   delay(5000);
-
-// check if returns are valid, if they are NaN (not a number) then something went wrong!
+   delay(10000);
+   String sensorHumi = "Humi,"+String(h);
+   String sensorTemp = "Temp,"+String(t);
+   
 if (isnan(t) || isnan(h)) {
     Serial.println("Failed to read from DHT");
   } else {
@@ -39,4 +79,15 @@ if (isnan(t) || isnan(h)) {
     Serial.print(t);
     Serial.println(" *C");
   }
+
+  if (!client.connected()) {
+    reconnect();
+  }//if not connect
+  
+    client.publish(topic,(char *)sensorHumi.c_str());
+    delay(5000);
+
+    client.publish(topic,(char *)sensorTemp.c_str());
+    delay(5000);
+    client.loop();
 }
